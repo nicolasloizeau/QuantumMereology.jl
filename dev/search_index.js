308531a27f2150b4a72079817f1f4ae450ca2707
Documenter.jl <documenter@juliadocs.github.io>
2026-03-20 14:26:43 +0000

    build based on 61d2064

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#QuantumMereology","page":"Home","title":"QuantumMereology","text":"Documentation for QuantumMereology.\n\n","category":"section"}]
+}
