9325ea2e75a37a42c75890e236e73fc53c885dc2
Documenter.jl <documenter@juliadocs.github.io>
2026-03-20 15:30:50 +0000

    build based on 4a27855

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#QuantumMereology","page":"Home","title":"QuantumMereology","text":"Documentation for QuantumMereology.\n\n","category":"section"}]
+[{"location":"#QuantumMereology","page":"Home","title":"QuantumMereology","text":"Documentation for QuantumMereology.\n\n","category":"section"},{"location":"#QuantumMereology.GOE-Tuple{Integer}","page":"Home","title":"QuantumMereology.GOE","text":"GOE(N::Integer)\n\nTraceless Gaussian Orthogonal Ensemble (GOE) random matrix of size 2^N x 2^N.\n\n\n\n\n\n","category":"method"}]
 }
